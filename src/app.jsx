2ed877d21d1320cc59ec00e6f7fb324c67c1e70e
Travezripley <theripleys@gmail.com>
2019-04-04 11:41:29 -0700

    updates html, adds axios and updates app,jsx with constructors to pull in data, adds initial now.json file

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+  class App extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        topspots: []
+      };
+    }
+  }
+
+
+  
+
   render() {
     return (
-      <div className='App'></div>
+   
+      <div className='App'>
+
+        <div className="container">
+          <div className="jumbotron jumbotron-fluid padding-top-4">
+            <h1 className="display-4">San Diego Top Spots</h1>
+            <p className="lead">A list of the Top 30 places to see in San Diego, California.</p>
+          </div>
+        </div>
+       
+      </div >
+    
     );
   }
 }
