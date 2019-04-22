7ad2febb18a5a7637d7bc0ef4d6bf9cf4d5476e9
Travez Ripley <theripleys@gmail.com>
2019-04-22 16:32:54 -0700

    Ready for Deployment

--- src/app.jsx
+++ src/app.jsx
@@ -13,37 +13,33 @@ class App extends Component {
 
   componentWillMount() {
     axios
-      .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
-      .then(response => response.data)
-      .then(topspots => this.setState({ topspots }));
+            .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+            .then(response => response.data)
+            .then(topspots => this.setState({ topspots }));
   }
 
   render() {
     return (
       <div className='App'>
-
-        <div className="container">
-          <div className="jumbotron jumbotron-fluid padding-top-4">
-            <h1 className="display-4">San Diego Top Spots</h1>
-            <p className="lead">A list of the Top 30 places to see in San Diego, California.</p>
+        <div className='container'>
+          <div className='jumbotron jumbotron-fluid padding-top-4'>
+            <h1 className='display-4'>San Diego Top Spots</h1>
+            <p className='lead'>
+                            A list of the Top 30 places to see in San Diego, California.
+                        </p>
           </div>
-          {
-            this.state.topspots.map(topspot => (
-              <TopSpot
-                key={topspot.id}
-                name={topspot.name}
-                description={topspot.description}
-                location={topspot.location} />
-
-            ))
-          }
+          {this.state.topspots.map(topspot => (
+            <TopSpot
+              key={ topspot.id }
+              name={ topspot.name }
+              description={ topspot.description }
+              location={ topspot.location }
+            />
+                    ))}
           {/* Below commented out code was to initially check that data was coming in from api */}
-          {/*<pre>{ JSON.stringify(this.state.topspots, null, 2) }</pre>*/}
+          {/* <pre>{ JSON.stringify(this.state.topspots, null, 2) }</pre>*/}
         </div>
-
-
-      </div >
-
+      </div>
     );
   }
 }
--- src/topspot.jsx
+++ src/topspot.jsx
@@ -1,16 +1,16 @@
-//this is a stateless component for your pleasure.
+// this is a stateless component for your pleasure.
 import React from 'react';
 
 export default props => (
-
-  <div className="well">
+  <div className='well'>
     <h4>{props.name}</h4>
     <p>{props.description}</p>
-  <a
-  href={`https://maps.google.com/?q=` + props.location[0] + ',' + props.location[1]}
-  target='blank'
-  className="btn btn-primary">Open in map</a>
-
+    <a
+      href={ `https://maps.google.com/?q=${props.location[0]},${props.location[1]}` }
+      target='blank'
+      className='btn btn-primary'
+    >
+            Open in map
+        </a>
   </div>
-
 );
